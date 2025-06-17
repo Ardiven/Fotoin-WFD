6fd20804b86d7bed19923aef71f644569d0dee50
Victor Loka <ardiven.lentera@gmail.com>
2025-06-17 19:12:34 +0700

    docker

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,24 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
 export default defineConfig({
-    plugins: [
+  plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
     ],
+  build: {
+    outDir: 'public/build',
+    manifest: true,
+    rollupOptions: {
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+    },
+  },
 });
+
+
